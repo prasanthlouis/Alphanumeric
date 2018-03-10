f5e125890cc495bea3be0b6d9decd96d8fadf7ef
unknown <prasanthlouis21@gmail.com>
2018-03-09 22:11:54 -0500

    Adding label with textbox to hold number

--- AlphaPhoneConverter/AlphaPhoneConverter/MainPage.xaml.cs
+++ AlphaPhoneConverter/AlphaPhoneConverter/MainPage.xaml.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace AlphaPhoneConverter
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var stackLayout = new StackLayout();
+            var label = new Label
+            {
+                Text = "Please enter an alphanumeric string",
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center
+            };
+            stackLayout.Children.Add(label);
+            var entry = new Entry();
+            stackLayout.Children.Add(entry);
+            var scrollViewer = new ScrollView
+            {
+                Content = stackLayout,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+
+            
+        }
+    }
 }
